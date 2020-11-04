af810845f5805237581305e85f6673c9b7905c96
iromashko <iromashko@me.com>
2020-11-04 21:00:30 +0300

    fix

--- public/script.js
+++ public/script.js
@@ -12,8 +12,8 @@ function sendData(e) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      name: formData.get('email'),
-      email: formData.get('name'),
+      name: formData.get('name'),
+      email: formData.get('email'),
       age: formData.get('age'),
       zip: formData.get('zip'),
       phone1: formData.get('phone1'),
@@ -28,7 +28,10 @@ function sendData(e) {
     .then((data) => {
       if (data.success) {
         console.log(`successful`);
+        document.querySelector('.errorContainer').style.display = 'none';
+        document.querySelector('.successContainer').style.display = 'block';
       } else {
+        document.querySelector('.successContainer').style.display = 'none';
         let error = document.querySelector('.error');
 
         error.innerHTML = '';
